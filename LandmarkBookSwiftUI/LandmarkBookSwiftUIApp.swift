a9872f2314f5be77bce9d05b28e99f6d536f3022
Ömer Apaydın <omerapaydin90@gmail.com>
2024-11-21 18:59:25 +0300

    Initial Commit

---
+++ LandmarkBookSwiftUI/LandmarkBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkBookSwiftUIApp.swift
+//  LandmarkBookSwiftUI
+//
+//  Created by Ömer on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarkBookSwiftUITests/LandmarkBookSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkBookSwiftUITests.swift
+//  LandmarkBookSwiftUITests
+//
+//  Created by Ömer on 21.11.2024.
+//
+
+import Testing
+@testable import LandmarkBookSwiftUI
+
+struct LandmarkBookSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
