acae4ade1d5c4cc7ce7451583ca6cb47383983b9
Ömer Apaydın <omerapaydin90@gmail.com>
2024-11-22 15:58:01 +0300

    Second

---
+++ LandmarkBookSwiftUI/LandmarkListRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkListRow.swift
+//  LandmarkBookSwiftUI
+//
+//  Created by Ömer on 22.11.2024.
+//
+
+import SwiftUI
+
+struct LandmarkListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkListRow()
+}
---
+++ LandmarkBookSwiftUI/LandmarkListRowImage.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkListRowImage.swift
+//  LandmarkBookSwiftUI
+//
+//  Created by Ömer on 22.11.2024.
+//
+
+import SwiftUI
+
+struct LandmarkListRowImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkListRowImage()
+}
